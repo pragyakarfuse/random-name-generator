0a8dc9c113b35327b9f200783999c7cfbc28901d
pragyakarfuse <pragyakar@fusemachines.com>
2020-03-31 23:32:32 +0545

    Updates

---
+++ precache-manifest.74a77f660482784765a6fff1c6fa72ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8175ecaf1c3527a3280a313d9731bc",
+    "url": "/random-name-generator/index.html"
+  },
+  {
+    "revision": "ae7d88f8e92269a27b6d",
+    "url": "/random-name-generator/static/css/main.46e950ad.chunk.css"
+  },
+  {
+    "revision": "fbee654bd9453e89c3c8",
+    "url": "/random-name-generator/static/js/2.cbc7c15c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-name-generator/static/js/2.cbc7c15c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae7d88f8e92269a27b6d",
+    "url": "/random-name-generator/static/js/main.2ffb7e42.chunk.js"
+  },
+  {
+    "revision": "77d30ca2ee14f02daae2",
+    "url": "/random-name-generator/static/js/runtime-main.242e476f.js"
+  }
+]);
